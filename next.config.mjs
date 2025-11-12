cab8a96999b48b4060f5cffe83bc6ba5887d9965
cctofu <calvinchang216@gmail.com>
2025-11-11 23:36:08 -0500

    updated portfolio

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   output: 'export',
   eslint: {
@@ -10,6 +12,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: '', // No basePath needed for user/org GitHub Pages (cctofu.github.io)
+  assetPrefix: '', // No assetPrefix needed for user/org GitHub Pages
 }
 
 export default nextConfig
