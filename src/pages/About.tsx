35b038181e33272b3bcb670f73871cb3d6839ecc
cctofu <calvinchang216@gmail.com>
2026-01-25 17:10:52 -0500

    fixed grammar issue

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,7 +4,7 @@ function About() {
   return (
     <div className="about">
       <p className="about-text">
-        Hi I'm Calvin, currently a master's student at <span className="highlight">@Columbia</span>, I previously studied at <span className="highlight">@Tsinghua</span> and worked at Companies like <span className="highlight">@Tecent</span> and also did research projects on usage of LLMs within the Legal Industry.
+        Hi I'm Calvin, currently a master's student at <span className="highlight">@Columbia</span>, I previously studied at <span className="highlight">@Tsinghua</span> and worked at Companies like <span className="highlight">@Tencent</span> and also did research projects on usage of LLMs within the Legal Industry.
       </p>
       <p className="about-body">
         I enjoy building small projects that help me learn more about new software and technologies out there. Lately, I’ve been especially interested in exploring how AI can be used within the gaming industry.
